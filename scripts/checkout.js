9e9be0a5e5912ee7f7d291aa2098991fcd9a5a28
a-negi1 <adheeshnegi16@gmail.com>
2026-01-10 23:19:40 +0530

    checkout delete button fixed

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { formatCurrency } from "./utils/money.js";
 import { products } from "../data/products.js";
 let cartSummaryHTML = '';
@@ -15,7 +15,7 @@ cart.forEach((cartItem) => {
     }
   });
     cartSummaryHTML+=
-    ` <div class="cart-item-container">
+    ` <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -95,9 +95,13 @@ cart.forEach((cartItem) => {
 
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
-document.querySelectorAll('.link-delete-link').forEach((link) => {link.addEventListener('click',()=>{
+document.querySelectorAll('.link-delete-link').forEach((link) => {link.addEventListener('click', () => {
   const productId = link.dataset.productId;
-  console.log(productId);
+  removeFromCart(productId);
+  const container = document.querySelector(`.js-cart-item-container-${productId} `);
+  container.remove();
 })})
+
+
 
 
